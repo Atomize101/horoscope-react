7673971e9775406e32764afcd85f40176aba597f
Chris Patrick <atomize101@gmail.com>
2020-10-19 11:22:38 -0400

    Breaking up the set sign component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import { getSigns } from './service/api';
+import { SelectSign } from './components/SelectSign';
 
 function App() {
-	const [signs, setSigns] = useState([]);
 	const [selectedSign, setSelectedSign] = useState(null);
-
-	useEffect(() => {
-		getSigns().then(setSigns);
-	}, []);
 
 	return (
 		<div className="App">
 			<h1>The Horoscope App</h1>
-			<h2>Please select a sign</h2>
 			{selectedSign}
-			<div className="grid">
-				{signs.map((sign) => (
-					<button className="sign" key={sign} onClick={() => setSelectedSign(sign)}>
-						{sign}
-					</button>
-				))}
-			</div>
+			<SelectSign onSignSelected={setSelectedSign} />
 		</div>
 	);
 }
